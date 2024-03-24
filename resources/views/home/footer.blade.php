1e6547f6424d2c9fee14ba6fde36620148114083
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-03-24 19:06:28 +0300

    footer fix

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -9,6 +9,10 @@
             <!-- Helpful links -->
             <div class="col d-flex align-items-center">
                 <div class="d-flex">
+                    <a href="javascript:void(0);" class="d-block mb-3">
+                        <img class="img-fluid wow fadeInUp landing-logo"
+                            src="{{$logo}}" alt="">
+                    </a>
                     <a href="#" class="text-decoration-none me-3">Home</a>
                     <a href="#features" class="text-decoration-none me-3">Features</a>
                     <a href="#contact" class="text-decoration-none me-3">Contact</a>
@@ -18,10 +22,7 @@
 
             <!-- Logo and copyright -->
             <div class="col text-end">
-                <a href="javascript:void(0);" class="d-block mb-3">
-                    <img class="img-fluid wow fadeInUp landing-logo"
-                        src="{{$logo}}" alt="">
-                </a>
+
                 <p class="mb-0">{{ __('Copyright') }} {{ date('Y') }} {{ env('APP_NAME') }}</p>
             </div>
         </div>
